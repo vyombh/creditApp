8c93f58018f11fbfad2cbb3b69fd288dacad44f2
vyombh <vyombh@gmail.com>
2017-10-01 17:58:43 +0530

    approval status update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,20 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!
   def index
   end
+  def approval
+
+  	id = params[:id]
+cd = Creditdetail.find_by_id(id);
+  	
+  	user = User.find_by_id(cd.user_id)
+  	if user.cl >= cd.amount
+  	user.cl = user.cl - cd.amount
+  	cd.requestStatus = 1
+  	cd.save
+  	user.save
+  	end
+  	
+  	return redirect_to '/'
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :creditdetails
   get 'home/index'
   root 'home#index'
-
+  post '/approval' => 'home#approval'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
